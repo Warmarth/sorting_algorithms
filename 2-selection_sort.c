3e8f6ce29fc92031fd041bbec74a52628c3dcdc0
Warmarth <godsonibeaka@gmail.com>
2022-11-09 03:27:15 -0800

    update

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -34,9 +34,8 @@ void selection_sort(int *array, size_t size)
 	{
 		min = array + i;
 		for (j = i + 1; j < size; j++)
-			if (array[j] < array[i + 1])
-				*min = j;
-
+			min = (array[j] < *min) ? (array + j) : min;
+			
 		if ((array + i) != min)
 		{
 			swap_integer(array + i, min);
